664d15d0a6c24d093e2519a1ccf115d5054fe5fc
_juicefv <alexander.kasyan@diginetica.com>
2022-03-23 15:48:59 +0100

    DS-138: Add debug on exception w/ interactive mode

---
+++ source/core/debug.py
@@ -0,0 +1,31 @@
+import sys
+import abc
+from types import TracebackType
+from typing import Optional, Type
+
+
+class BaseDebugClass(abc.ABC):
+    @abc.abstractstaticmethod
+    def start() -> None:
+        pass
+
+
+class DebugOnException(BaseDebugClass):
+    @staticmethod
+    def start() -> None:
+        def info(
+            type: Type[BaseException],
+            value: BaseException,
+            traceback: Optional[TracebackType]
+        ) -> None:
+            if hasattr(sys, "ps1") or not sys.stderr.isatty():
+                sys.__excepthook__(type, value, traceback)
+            else:
+                import pdb
+                import traceback
+
+                traceback.print_exception(type, value, traceback)
+                print
+                pdb.post_mortem(traceback)
+
+        sys.excepthook = info
