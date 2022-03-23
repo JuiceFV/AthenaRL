8f70baf2be1966a4a1955414b42746d221808bb3
_juicefv <alexander.kasyan@diginetica.com>
2022-03-23 15:48:14 +0100

    DS-138: Add typed dataclasses

---
+++ source/core/dataclasses.py
@@ -0,0 +1,52 @@
+import dataclasses
+import os
+import pydantic
+
+from dataclasses import field  # noqa
+from typing import TYPE_CHECKING, Any, Optional
+
+
+USE_VANILLA_DATACLASS = bool(int(os.environ.get("USE_VANILLA_DATACLASS", False)))
+ARBITRARY_TYPES_ALLOWED = bool(int(os.environ.get("ARBITRARY_TYPES_ALLOWED", True)))
+
+
+if TYPE_CHECKING:
+    from dataclasses import dataclass
+else:
+    def dataclass(_cls: Optional[Any] = None, *, config=None, **kwargs):
+        def wrap(cls):
+            # We don't want to look at parent class
+            if "__post_init__" in cls.__dict__:
+                raise TypeError(
+                    f"{cls} has __post_init__. "
+                    "Please use __post_init_post_parse__ instead."
+                )
+
+            if USE_VANILLA_DATACLASS:
+                try:
+                    post_init_post_parse = cls.__dict__[
+                        "__post_init_post_parse__"]
+                    logger.info(
+                        f"Setting {cls.__name__}.__post_init__ to its "
+                        "__post_init_post_parse__"
+                    )
+                    cls.__post_init__ = post_init_post_parse
+                except KeyError:
+                    pass
+
+                return dataclasses.dataclass(**kwargs)(cls)
+            else:
+                if ARBITRARY_TYPES_ALLOWED:
+
+                    class Config:
+                        arbitrary_types_allowed = ARBITRARY_TYPES_ALLOWED
+
+                    assert config not in kwargs
+                    kwargs["config"] = Config
+
+                return pydantic.dataclasses.dataclass(cls, **kwargs)
+
+        if _cls is None:
+            return wrap
+
+        return wrap(_cls)
