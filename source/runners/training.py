8702f80001bf823eee177da0f174624361800785
_juicefv <alexander.kasyan@diginetica.com>
2022-03-29 13:58:08 +0200

    DS-138: Add configs directoy

---
+++ source/runners/training.py
@@ -0,0 +1,9 @@
+from source.model_managers.roster import ModelManagerRoster
+from source.core.dtypes import TableSpec
+
+
+def build_and_train(
+    input_table_spec: TableSpec,
+    model: ModelManagerRoster
+):
+    pass
