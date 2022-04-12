fc91711502474feadef0ea0b1fe15e0447409135
_juicefv <alexander.kasyan@diginetica.com>
2022-04-12 18:14:56 +0200

    DS-138: Add episodes buffer and its sequence representation

---
+++ source/episode/__init__.py
@@ -0,0 +1,4 @@
+from source.episode.episodes_buffer import EpisodesBuffer
+from source.episode.sequence import Sequence, SeqenceEntity
+
+__all__ = ["Sequence", "SeqenceEntity", "EpisodesBuffer"]
---
+++ source/episode/episodes_buffer/__init__.py
@@ -0,0 +1,3 @@
+from source.episode.episodes_buffer.buffer import EpisodesBuffer
+
+__all__ = ["EpisodesBuffer"]
