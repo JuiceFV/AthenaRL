0d0d7b13461763eca9b31d57d1186adc083f7e51
_juicefv <alexander.kasyan@diginetica.com>
2022-07-05 15:06:19 +0200

    DS-138: add Seq2SlateVersion

--- athena/core/dtypes/ranking/__init__.py
+++ athena/core/dtypes/ranking/__init__.py
@@ -2,7 +2,9 @@ from athena.core.dtypes.ranking.base import (DocSeq, PreprocessedRankingInput,
                                              RankingOutput)
 from athena.core.dtypes.ranking.seq2slate import (Seq2SlateMode,
                                                   Seq2SlateOutputArch,
-                                                  Seq2SlateTransformerOutput)
+                                                  Seq2SlateTransformerOutput,
+                                                  Seq2SlateVersion)
 
 __all__ = ["DocSeq", "RankingOutput", "PreprocessedRankingInput",
-           "Seq2SlateMode", "Seq2SlateOutputArch", "Seq2SlateTransformerOutput"]
+           "Seq2SlateMode", "Seq2SlateOutputArch", "Seq2SlateTransformerOutput",
+           "Seq2SlateVersion"]
