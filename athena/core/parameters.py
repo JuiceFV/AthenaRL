30e4fa8ed4dee9be9e50b0d74885a8c5c6ce08cc
_juicefv <alexander.kasyan@diginetica.com>
2022-08-07 22:06:04 +0200

    DS-138: Add NormalizationParams and NormalizationData

--- athena/core/parameters.py
+++ athena/core/parameters.py
@@ -1,9 +1,13 @@
-from typing import Optional
+from typing import Dict, List, Optional
 
 import athena.core.dtypes as adt
 from athena.core.base_dclass import BaseDataClass
 from athena.core.config import param_hash
 from athena.core.dataclasses import dataclass
+from athena.preprocessing.identify_types import Ftype
+
+SortedList = List[float]
+EnumValues = List[int]
 
 
 @dataclass(frozen=True)
@@ -16,7 +20,28 @@ class TransformerParams(BaseDataClass):
 
 
 @dataclass(frozen=True)
-class Seq2SlateParameters(BaseDataClass):
+class Seq2SlateParams(BaseDataClass):
     on_policy: bool = True
     version: adt.Seq2SlateVersion = adt.Seq2SlateVersion.REINFORCEMENT_LEARNING
     ips_blur: Optional[adt.IPSBlur] = None
+
+
+@dataclass(frozen=True)
+class NormalizationParams(BaseDataClass):
+    __hash__ = param_hash
+
+    ftype: Ftype
+    boxcox_lambda: Optional[float] = None
+    boxcox_shift: Optional[float] = None
+    mean: Optional[float] = None
+    stdev: Optional[float] = None
+    possible_values: Optional[EnumValues] = None
+    quantiles: Optional[SortedList] = None
+    min_value: Optional[float] = None
+    max_value: Optional[float] = None
+
+
+@dataclass(frozen=True)
+class NormalizationData(BaseDataClass):
+    __hash__ = param_hash
+    dense_normalization_params: Dict[int, NormalizationParams]
--- athena/core/parameters/__init__.py
+++
@@ -1,5 +0,0 @@
-from athena.core.config import param_hash
-
-from athena.core.parameters.transformer import TransformerParams
-
-__all__ = ["TransformerParams", "param_hash"]
