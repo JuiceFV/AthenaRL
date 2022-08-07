1c9b59905850d8b6bc7160ff868b27773c8b784a
_juicefv <alexander.kasyan@diginetica.com>
2022-08-07 23:09:14 +0200

    DS-138: Add seq2slate trainer params

--- athena/trainers/__init__.py
+++ athena/trainers/__init__.py
@@ -1,3 +1,4 @@
 from athena.trainers.athena_lightening import AthenaLightening, STEP_OUTPUT
-
-__all__ = ["AthenaLightening", "STEP_OUTPUT"]
+from athena.trainers.ranking.seq2slate.seq2slate_base import Seq2SlateTrainer
+from athena.trainers.parameters import Seq2SlateTrainerParameters
+__all__ = ["AthenaLightening", "STEP_OUTPUT", "Seq2SlateTrainer", "Seq2SlateTrainerParameters"]
---
+++ athena/trainers/parameters.py
@@ -0,0 +1,9 @@
+from athena.core.config import create_config_class
+from athena.core.dtypes import BaseDataClass
+
+from athena.trainers.ranking.seq2slate.seq2slate_base import Seq2SlateTrainer
+
+
+@create_config_class(Seq2SlateTrainer.__init__, blocklist=["reinforce_network", "baseline_network", "baseline_warmup_batches"])
+class Seq2SlateTrainerParameters(BaseDataClass):
+    pass
