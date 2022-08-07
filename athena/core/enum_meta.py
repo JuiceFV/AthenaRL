fb4ec30565943a8d4a2be28f2324bada6dd6c97a
_juicefv <alexander.kasyan@diginetica.com>
2022-08-07 22:05:00 +0200

    DS-138: Add AthenaEnumMeta

---
+++ athena/core/enum_meta.py
@@ -0,0 +1,12 @@
+from typing import Union
+from enum import EnumMeta, Enum
+
+__all__ = ["AthenaEnumMeta", "Enum"]
+
+class AthenaEnumMeta(EnumMeta):
+    def __contains__(cls, item: Union[Enum, str]) -> bool:
+        return item in list(cls._value2member_map_.keys()) + list(cls._value2member_map_.values())
+    
+    def item_index(cls, item: Union[Enum, str]) -> int:
+        str_item = item if isinstance(item, str) else item.value
+        return list(cls._value2member_map_).index(str_item)
