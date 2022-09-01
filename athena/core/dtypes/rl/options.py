82db0377342a0b4448dbe067bf4d73a96dad2ba1
_juicefv <alexander.kasyan@diginetica.com>
2022-09-01 13:49:58 +0200

    DS-138: Add reward fields and discount gamma

--- athena/core/dtypes/rl/options.py
+++ athena/core/dtypes/rl/options.py
@@ -4,8 +4,15 @@ from athena.core.dataclasses import dataclass
 
 @dataclass(frozen=True)
 class RewardOptions:
-    custom_reward_expression: Optional[str] = None
+    reward_col_name: str = "reward"
+    metrics_col_name: str = "metrics"
+    custom_reward: Optional[str] = None
     metric_reward_values: Optional[Dict[str, float]] = None
+    gamma: Optional[float] = None
+
+    def __post_init_post_parse__(self):
+        if self.custom_reward is not None and self.gamma is not None:
+            raise RuntimeError("The only option (either custom_reward or gamma) should be defined")
 
 
 @dataclass
