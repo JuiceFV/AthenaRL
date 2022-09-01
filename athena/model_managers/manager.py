a9e73289aa3339c45f942dbb44d771c648ec8402
_juicefv <alexander.kasyan@diginetica.com>
2022-09-01 14:14:00 +0200

    DS-138: replenish Manager

--- athena/model_managers/manager.py
+++ athena/model_managers/manager.py
@@ -1,8 +1,15 @@
-from typing import Optional
+import abc
+from typing import Dict, Optional, Tuple
 from athena.core.dataclasses import dataclass
+from athena.core.dtypes.dataset import Dataset
+from athena.core.dtypes.options import MLOptionsRoster, ReaderOptions, ResourceOptions
 from athena.core.logger import LoggerMixin
 from athena.core.dtypes import TableSpec
-from athena.data.base import DataModule
+from athena.core.parameters import NormalizationData
+from athena.data.data_extractor import DataExtractor
+from athena.data.manual_datamodule import ManualDataModule
+from athena.report.base import ReporterBase
+from athena.trainers.athena_lightening import AthenaLightening
 
 
 @dataclass
@@ -12,6 +19,44 @@ class ModelManager(LoggerMixin):
 
     def get_data_module(
         self,
-        input_table_spec: TableSpec
-    ) -> Optional[DataModule]:
+        *,
+        input_table_spec: Optional[TableSpec] = None,
+        ml_options: Optional[MLOptionsRoster] = None,
+        setup_data: Optional[Dict[str, bytes]] = None,
+        saved_setup_data: Optional[Dict[str, bytes]] = None,
+        reader_options: Optional[ReaderOptions] = None,
+        resource_options: Optional[ResourceOptions] = None
+    ) -> Optional[ManualDataModule]:
         return None
+
+    @abc.abstractmethod
+    def build_trainer(
+        self,
+        normalization_dict: Dict[str, NormalizationData],
+        use_gpu: bool,
+        ml_options: Optional[MLOptionsRoster] = None
+    ) -> AthenaLightening:
+        pass
+
+    @abc.abstractmethod
+    def get_reporter(self) -> ReporterBase:
+        pass
+
+    @abc.abstractmethod
+    def run_feature_identification(self, input_table_spec: TableSpec) -> Dict[str, NormalizationData]:
+        pass
+
+    @abc.abstractmethod
+    def query_data(
+        self,
+        input_table_spec: TableSpec,
+        sample_range: Optional[Tuple[float, float]],
+        ml_options: MLOptionsRoster,
+        data_extractor: DataExtractor
+    ) -> Dataset:
+        pass
+
+    @property
+    @abc.abstractmethod
+    def should_generate_eval_dataset(self) -> bool:
+        pass
--- athena/model_managers/seq2slate_base.py
+++ athena/model_managers/seq2slate_base.py
@@ -1,15 +1,28 @@
-from typing import Optional
+from typing import Optional, Dict
 
-from athena.core.dataclasses import dataclass
-from athena.data.base import DataModule
+from athena.core.dataclasses import dataclass, field
+from athena.core.parameters import NormalizationData
+from athena.data.athena_datamodule import AthenaDataModule
 from athena.core.dtypes import TableSpec
+from athena.core.dtypes.options import PreprocessingOptions
+from athena.data.manual_datamodule import ManualDataModule
 from athena.model_managers.manager import ModelManager
 
 
 @dataclass
 class Seq2SlateBase(ModelManager):
+
     def __post_init_post_parse__(self):
         super().__post_init_post_parse__()
 
-    def get_data_module(self, input_table_spec: TableSpec) -> Optional[DataModule]:
+    def get_data_module(self, input_table_spec: TableSpec) -> Optional[AthenaDataModule]:
         return super().get_data_module(input_table_spec)
+
+
+class Seq2SlateDataModule(ManualDataModule):  # Inherits ManualDataModule
+    @property
+    def should_generate_eval_dataset(self) -> bool:
+        return self.model_manager.eval_parameters.cpe_evaluation
+
+    def run_feature_identification(self, input_table_spec: TableSpec) -> Dict[str, NormalizationData]:
+        pass
