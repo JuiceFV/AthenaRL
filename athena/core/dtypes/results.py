debd8f4089f1aad15a538fcf2ddb86026b112d39
_juicefv <alexander.kasyan@diginetica.com>
2022-09-01 13:50:46 +0200

    DS-138: Add results reporter

--- athena/core/dtypes/results.py
+++ athena/core/dtypes/results.py
@@ -1,13 +1,7 @@
-from typing import Dict, List, Tuple
+from typing import Dict, List, Optional, Tuple
 
 from athena.core.dataclasses import dataclass, field
 from athena.core.registry import RegistryMeta, DiscriminatedUnion
-
-
-@dataclass
-class TrainingOutput:
-    output_paths: Dict[str, str] = field(default_factory=dict)
-    logger_data: Dict[str, Dict[str, List[Tuple[float, float]]]] = field(default_factory=dict)
 
 
 @dataclass
@@ -38,3 +32,26 @@ class NoPublishingResults(PublishingResult):
 @PublishingResult.register()
 class PublishingResultRoster(DiscriminatedUnion):
     pass
+
+
+@dataclass
+class TrainingReport(metaclass=RegistryMeta):
+    pass
+
+
+@dataclass
+class Seq2SlateTrainingReport(TrainingReport):
+    __registry_name__ = "seq2slate_report"
+
+
+class TrainingReportRoster(DiscriminatedUnion):
+    pass
+
+
+@dataclass
+class TrainingOutput:
+    output_paths: Dict[str, str] = field(default_factory=dict)
+    validation_result: Optional[ValidationResultRoster] = None
+    publishing_result: Optional[PublishingResultRoster] = None
+    training_report: Optional[TrainingReport] = None
+    logger_data: Dict[str, Dict[str, List[Tuple[float, float]]]] = field(default_factory=dict)
