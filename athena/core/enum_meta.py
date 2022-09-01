ff5a5976d8e74d22b2988c4d343b1fe53054efda
_juicefv <alexander.kasyan@diginetica.com>
2022-09-01 14:03:14 +0200

    DS-138: Style fix

--- athena/core/enum_meta.py
+++ athena/core/enum_meta.py
@@ -3,10 +3,11 @@ from enum import EnumMeta, Enum
 
 __all__ = ["AthenaEnumMeta", "Enum"]
 
+
 class AthenaEnumMeta(EnumMeta):
     def __contains__(cls, item: Union[Enum, str]) -> bool:
         return item in list(cls._value2member_map_.keys()) + list(cls._value2member_map_.values())
-    
+
     def item_index(cls, item: Union[Enum, str]) -> int:
         str_item = item if isinstance(item, str) else item.value
         return list(cls._value2member_map_).index(str_item)
