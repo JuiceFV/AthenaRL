78c23786b645bcfe0f47509cc901a5b883db90c2
_juicefv <alexander.kasyan@diginetica.com>
2022-09-22 21:08:17 +0200

    DS-138: Add Input Columns names

--- athena/core/dtypes/preprocessing/__init__.py
+++ athena/core/dtypes/preprocessing/__init__.py
@@ -1,5 +1,7 @@
+from athena.core.dtypes.preprocessing.base import InputColumn
 from athena.core.dtypes.preprocessing.options import PreprocessingOptions
 
 __all__ = [
-    "PreprocessingOptions"
+    "PreprocessingOptions",
+    "InputColumn"
 ]
