c4bd12be051d19c9db61c7fa898867ceba3d8f23
_juicefv <alexander.kasyan@diginetica.com>
2022-09-25 21:33:06 +0200

    DS-138: Add transforms for the petastorm make_batch_reader

---
+++ athena/preprocessing/transforms.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import torch.nn as nn
+import numpy as np
+from typing import Callable, List
+
+
+class Transformation(nn.Module):
+    def __init__(self, keys: List[str]) -> None:
+        super().__init__()
+        self.keys = keys
+
+
+class Compose:
+    def __init__(self, transformations: List[Transformation]) -> None:
+        self.transformations = transformations
+
+    def __call__(self, data: pd.DataFrame) -> pd.DataFrame:
+        for trans in self.transformations:
+            data = trans(data)
+        return data
+
+
+class Lambda(Transformation):
+    def __init__(self, keys: List[str], fn: Callable[[pd.Series], pd.Series]) -> None:
+        super().__init__(keys)
+        self.fn = fn
+
+    def forward(self, data: pd.DataFrame) -> pd.DataFrame:
+        for key in self.keys:
+            data[key] = self.fn(data[key])
+        return data
+
+
+class VectorPadding(Transformation):
+    def __init__(self, keys: List[str], max_lengths: List[int]) -> None:
+        super().__init__(keys)
+        if len(keys) != len(max_lengths):
+            raise RuntimeError(
+                f"Expected max_length for each vector column; Got {len(keys)} != {len(max_lengths)}"
+            )
+        self.max_lengths = max_lengths
+
+    def forward(self, data: pd.DataFrame) -> pd.DataFrame:
+        for key, max_len in zip(self.keys, self.max_lengths):
+            data[key] = data[key].map(
+                lambda vec: np.pad(
+                    vec, (0, max_len - len(vec)),
+                    'constant', constant_values=vec.dtype.type(0)
+                )
+            )
+        return data
