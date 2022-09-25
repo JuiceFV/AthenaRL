d520402f0a9f9771727f47f74ba43176b2a8a56f
_juicefv <alexander.kasyan@diginetica.com>
2022-09-25 21:53:44 +0200

    DS-138: Add base methods

--- athena/model_managers/manager.py
+++ athena/model_managers/manager.py
@@ -1,13 +1,17 @@
 import abc
-from typing import Dict, Optional, Tuple
+import torch
+import pytorch_lightning as pl
+from typing import Dict, List, Optional, Tuple
 from athena.core.dataclasses import dataclass
 from athena.core.dtypes.dataset import Dataset
-from athena.core.dtypes.options import MLOptionsRoster, ReaderOptions, ResourceOptions
+from athena.core.dtypes.options import RLOptions, ReaderOptions, ResourceOptions
 from athena.core.logger import LoggerMixin
 from athena.core.dtypes import TableSpec
+from athena.core.dtypes.results import TrainingOutput
 from athena.core.parameters import NormalizationData
+from athena.data.athena_datamodule import AthenaDataModule
 from athena.data.data_extractor import DataExtractor
-from athena.data.manual_datamodule import ManualDataModule
+from athena.data.fap.fapper import FAPper
 from athena.report.base import ReporterBase
 from athena.trainers.athena_lightening import AthenaLightening
 
@@ -19,22 +23,21 @@ class ModelManager(LoggerMixin):
 
     def get_data_module(
         self,
+        fapper: FAPper,
         *,
         input_table_spec: Optional[TableSpec] = None,
-        ml_options: Optional[MLOptionsRoster] = None,
+        rl_options: Optional[RLOptions] = None,
         setup_data: Optional[Dict[str, bytes]] = None,
         saved_setup_data: Optional[Dict[str, bytes]] = None,
         reader_options: Optional[ReaderOptions] = None,
         resource_options: Optional[ResourceOptions] = None
-    ) -> Optional[ManualDataModule]:
+    ) -> Optional[AthenaDataModule]:
         return None
 
     @abc.abstractmethod
     def build_trainer(
         self,
         normalization_dict: Dict[str, NormalizationData],
-        use_gpu: bool,
-        ml_options: Optional[MLOptionsRoster] = None
     ) -> AthenaLightening:
         pass
 
@@ -43,7 +46,7 @@ class ModelManager(LoggerMixin):
         pass
 
     @abc.abstractmethod
-    def run_feature_identification(self, input_table_spec: TableSpec) -> Dict[str, NormalizationData]:
+    def run_feature_identification(self, input_table_spec: TableSpec, fapper: FAPper) -> Dict[str, NormalizationData]:
         pass
 
     @abc.abstractmethod
@@ -51,10 +54,44 @@ class ModelManager(LoggerMixin):
         self,
         input_table_spec: TableSpec,
         sample_range: Optional[Tuple[float, float]],
-        ml_options: MLOptionsRoster,
+        rl_options: RLOptions,
         data_extractor: DataExtractor
     ) -> Dataset:
         pass
+
+    def train(
+        self,
+        trainer_module: AthenaLightening,
+        train_dataset: Optional[Dataset],
+        eval_dataset: Optional[Dataset],
+        test_dataset: Optional[Dataset],
+        data_module: Optional[AthenaDataModule],
+        num_epochs: int,
+        reader_options: ReaderOptions,
+        resource_options: ResourceOptions,
+        checkpoint_path: Optional[str] = None
+    ) -> Tuple[TrainingOutput, pl.Trainer]:
+        reporter = self.get_reporter()
+        trainer_module.set_reporter(reporter)
+        if data_module is None:
+            raise ValueError("No datamodule provided")
+
+    def build_serving_modules(
+        self,
+        trainer_module: AthenaLightening,
+        normalization_dict: Dict[str, NormalizationData]
+    ) -> Dict[str, torch.nn.Module]:
+        return {"default_model": self.build_serving_module(trainer_module, normalization_dict)}
+
+    def build_serving_module(
+        self,
+        trainer_module: AthenaLightening,
+        normalization_dict: Dict[str, NormalizationData]
+    ) -> torch.nn.Module:
+        raise NotImplementedError
+
+    def serving_module_names(self) -> List[str]:
+        return ["default_model"]
 
     @property
     @abc.abstractmethod
