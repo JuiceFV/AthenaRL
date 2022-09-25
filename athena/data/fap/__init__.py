3e89147057009afe31053b5606ca356205443170
_juicefv <alexander.kasyan@diginetica.com>
2022-09-25 21:51:07 +0200

    DS-138: import fix

--- athena/data/fap/__init__.py
+++ athena/data/fap/__init__.py
@@ -1,9 +1,9 @@
 from athena.data.fap import spark_utils
-from athena.data.fap.spark import RLSparkFapper
+from athena.data.fap.spark import SparkFapper
 from athena.data.fap.roster import FAPperRoster
 
 __all__ = [
     "spark_utils",
-    "RLSparkFapper",
+    "SparkFapper",
     "FAPperRoster"
 ]
