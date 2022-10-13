326366eb68deda9f151d2a24877512bf3f891576
_juicefv <alexander.kasyan@diginetica.com>
2022-10-13 08:47:32 +0200

    DS-138: Add docs

--- athena/core/dtypes/dataset.py
+++ athena/core/dtypes/dataset.py
@@ -5,12 +5,35 @@ from athena.core.dataclasses import dataclass
 
 @dataclass
 class Dataset:
+    r"""
+    Preprocessed dataset.
+    """
+    #: Parquet link to the dataset.
     parquet_url: str
 
 
 @dataclass
 class TableSpec:
+    r"""
+    Table specification used for training.
+
+    .. note::
+
+        Currently, ``test_table_sample`` is the same as
+        ``eval_table_sample``.
+
+    .. important::
+
+        ``able_sample + eval_table_sample == 100``
+    """
+    #: Name of a table.
     table_name: str
+    #: Portion of entire table used to learn.
+    #: The value should lie within interval :math:`[0, 100]`
     table_sample: Optional[float] = None
+    #: Portion of entire table used to evaluate.
+    #: The value should lie within interval :math:`[0, 100]`
     eval_table_sample: Optional[float] = None
+    #: Portion of entire table used to test.
+    #: The value should lie within interval :math:`[0, 100]`
     test_table_sample: Optional[float] = None
--- athena/core/dtypes/options.py
+++ athena/core/dtypes/options.py
@@ -1,21 +1,39 @@
 from typing import Optional
+
 from athena.core.dataclasses import dataclass
-from athena.core.dtypes.rl import RLOptions
 from athena.core.dtypes.preprocessing import PreprocessingOptions
+from athena.core.dtypes.rl import RLOptions
 
 
 @dataclass
 class ReaderOptions:
+    r"""
+    Petastorm batch reader options.
+    """
+    #: Batch size.
     minibatch_size: int = 1024
+    #: A string denoting the reader pool type. Should
+    #: be one of ``['thread', 'process', 'dummy']``
     petasorm_reader_pool_type: str = "thread"
 
 
 @dataclass
 class ResourceOptions:
+    r"""
+    Resource specification used for learining.
+    """
+    #: How many GPU use. If no CUDA available
+    #: set it to 0.
     gpu: int = 0
 
     @property
     def use_gpu(self) -> bool:
+        r"""
+        Whether use GPU or not.
+
+        Returns:
+            bool: Whether use GPU or not.
+        """
         return self.gpu > 0
 
 
