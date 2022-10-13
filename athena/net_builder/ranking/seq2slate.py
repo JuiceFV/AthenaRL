d6941987e686c8291b680e697fad085e185e972b
_juicefv <alexander.kasyan@diginetica.com>
2022-10-13 11:18:17 +0200

    DS-138: Some modifications in network initialization

--- athena/net_builder/ranking/seq2slate.py
+++ athena/net_builder/ranking/seq2slate.py
@@ -1,9 +1,11 @@
 from athena.core.dataclasses import dataclass, field
 from athena.core.dtypes import Seq2SlateOutputArch
-from athena.core.parameters import NormalizationData, TransformerParams, param_hash
+from athena.core.parameters import (NormalizationData, TransformerParams,
+                                    param_hash)
 from athena.models import Seq2SlateTransformerNetwork
 from athena.net_builder.slate_ranking_builder import SlateRankingNetBuilder
-from athena.prediction.ranking.slate_predictor import Seq2SlateWithPreprocessor, Seq2SlatePredictorWrapper
+from athena.prediction.ranking.slate_predictor import (
+    Seq2SlatePredictorWrapper, Seq2SlateWithPreprocessor)
 from athena.preprocessing.preprocessor import Preprocessor
 
 
@@ -20,10 +22,10 @@ class Seq2SlateRanking(SlateRankingNetBuilder):
     )
 
     def build_slate_ranking_network(
-        self, latent_state_dim: int, candidate_dim: int, num_of_candidates: int, slate_size: int
+        self, state_dim: int, candidate_dim: int, num_of_candidates: int, slate_size: int
     ) -> Seq2SlateTransformerNetwork:
         return Seq2SlateTransformerNetwork(
-            latent_state_dim=latent_state_dim,
+            state_dim=state_dim,
             candidate_dim=candidate_dim,
             nlayers=self.transformer.nlayers,
             dim_model=self.transformer.dim_model,
@@ -33,7 +35,7 @@ class Seq2SlateRanking(SlateRankingNetBuilder):
             temperature=self.temperature,
             nheads=self.transformer.nheads,
             dim_feedforward=self.transformer.dim_feedforward,
-            latent_state_embed_dim=self.transformer.latent_state_embed_dim
+            state_embed_dim=self.transformer.state_embed_dim
         )
 
     def build_serving_module(
@@ -45,6 +47,6 @@ class Seq2SlateRanking(SlateRankingNetBuilder):
         state_preprocessor = Preprocessor(state_normalization_data.dense_normalization_params)
         candidate_preprocessor = Preprocessor(candidate_normalization_data.dense_normalization_params)
         seq2slate_with_preprocessor = Seq2SlateWithPreprocessor(
-            network.cpu_model().eval(), state_preprocessor, candidate_preprocessor
+            network.cpu_model().eval(), state_preprocessor, candidate_preprocessor, True
         )
         return Seq2SlatePredictorWrapper(seq2slate_with_preprocessor)
--- athena/net_builder/slate_ranking_builder.py
+++ athena/net_builder/slate_ranking_builder.py
@@ -1,14 +1,15 @@
 import abc
-import torch
-from athena.core.parameters import NormalizationData
 
+import torch
+
+from athena.core.parameters import NormalizationData
 from athena.models.base import BaseModel
 
 
 class SlateRankingNetBuilder:
     @abc.abstractmethod
     def build_slate_ranking_network(
-        self, latent_state_dim: int, candidate_dim: int, num_of_candidates: int, slate_size: int
+        self, state_dim: int, candidate_dim: int, num_of_candidates: int, slate_size: int
     ) -> torch.nn.Module:
         pass
 
