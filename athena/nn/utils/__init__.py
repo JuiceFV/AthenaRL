c12b82b0b9d6dff0ea11a21d9536322810d89f1d
_juicefv <alexander.kasyan@diginetica.com>
2023-06-16 12:49:56 +0200

    Add

--- athena/nn/utils/__init__.py
+++ athena/nn/utils/__init__.py
@@ -1,1 +1,7 @@
+import copy
+import torch.nn as nn
 from athena.nn.utils import transformer  # noqa
+
+
+def clones(module: nn.Module, ncopies: int) -> nn.ModuleList:
+    return nn.ModuleList([copy.deepcopy(module) for _ in range(ncopies)])
