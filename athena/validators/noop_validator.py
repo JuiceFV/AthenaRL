6acf8083e1345992c2194b9997d7668150070420
_juicefv <alexander.kasyan@diginetica.com>
2023-06-16 12:51:14 +0200

    style fix

--- athena/validators/noop_validator.py
+++ athena/validators/noop_validator.py
@@ -5,11 +5,12 @@ from athena.core.dtypes.results import NoValidationResults
 from athena.validators.validator_base import ModelValidator
 from athena.core.dtypes import TrainingOutput, TableSpec
 
+
 @dataclass
 class NoValidation(ModelValidator):
     def do_validate(
-        self, 
-        training_output: TrainingOutput, 
+        self,
+        training_output: TrainingOutput,
         history: Optional[List[TrainingOutput]] = None,
         input_table_spec: Optional[TableSpec] = None
     ) -> NoValidationResults:
